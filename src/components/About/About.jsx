733bb8a5f9e5267d8b38973c35930f573e83c5f6
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-10 00:30:46 +0200

    about section completed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
 import "./About.css";
 const About = () => {
   return (
     <section className="about">
-      <h1>Hi, I am Shehab Shalan</h1>
+      <h1>Hi 👋 , I am Shehab Shalan</h1>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi dolor
-        ex facere iure eveniet laudantium quisquam vel dolorem sequi! Dicta
-        cumque voluptatum explicabo, ab a suscipit beatae aliquam voluptate
-        ipsa!
+        I am a web developer currently pursuing a master's degree. I have worked
+        as a frontend developer for over a year. I have also worked on personal
+        projects involving mainly MERN stack. I have done mostly frontend
+        development, but I am also interested in backend development. I am
+        always open to new challenges.
       </p>
+      <div className="about-links">
+        <a href="#">
+          <FaGithub className="icon" size={30} />
+        </a>
+        <a href="#">
+          <FaLinkedinIn className="icon" size={30} />
+        </a>
+        <div className="btn">
+          <a href="#">Download CV</a>
+        </div>
+      </div>
     </section>
   );
 };
