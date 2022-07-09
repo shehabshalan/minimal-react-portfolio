d0cc2898a433db8b0f20e6ec54e366452b8216d2
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-10 00:31:06 +0200

    experience section completed

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import "./Experience.css";
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <section className="experience">
+      <h1>Experience</h1>
+      <div className="experience-container-grid">
+        <div className="experience-card">
+          <div className="job-title-duration-section">
+            <h2 className="job-title">
+              Frondend Developer - <span>Trusted KYB Limited</span>
+            </h2>
+            <p className="duration">03/2021 - 06/2022</p>
+          </div>
+          <p className="experience-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+            neque obcaecati praesentium perferendis magnam dignissimos cum quasi
+            dolore ducimus esse delectus modi corrupti soluta, minus assumenda
+            tempora sed! Consequatur, distinctio. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Eligendi, rem reiciendis! Cumque
+            neque commodi aliquam facere dolorem magnam assumenda voluptate
+            facilis, illum omnis perspiciatis fuga, totam corporis veritatis
+            maiores voluptatem?
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
