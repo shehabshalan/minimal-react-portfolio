b811eea15f6c2171285a2b6042618b4b04391664
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-09 17:38:45 +0200

    about section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./About.css";
+const About = () => {
+  return (
+    <section className="about">
+      <h1>Hi, I am Shehab Shalan</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi dolor
+        ex facere iure eveniet laudantium quisquam vel dolorem sequi! Dicta
+        cumque voluptatum explicabo, ab a suscipit beatae aliquam voluptate
+        ipsa!
+      </p>
+    </section>
+  );
+};
+
+export default About;
