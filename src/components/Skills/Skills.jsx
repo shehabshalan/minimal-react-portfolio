33f3693e39b6a66c71a50c4b7ab3f55611c4ae6e
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-10 00:31:38 +0200

    skills section completed

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import "./Skills.css";
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <section className="skills">
+      <h1>Skills</h1>
+      <div className="skills-container-grid">
+        <div className="skills-card">
+          <h3 className="skills-title">Languages & Frameworks</h3>
+          <p className="skills-stack">#React #Strapi #Node #Express #MongoDB</p>
+        </div>
+        <div className="skills-card">
+          <h3 className="skills-title">Databases</h3>
+          <p className="skills-stack">#React #Strapi #Node #Express #MongoDB</p>
+        </div>
+        <div className="skills-card">
+          <h3 className="skills-title">Others</h3>
+          <p className="skills-stack">#React #Strapi #Node #Express #MongoDB</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
