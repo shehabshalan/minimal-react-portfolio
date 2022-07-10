cfcfeca9014391886b1295d9b6de4b9149f7ce04
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-11 01:25:39 +0200

    get colors for stacks

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Skills.css";
 import { skills } from "../../data/portfolio";
+import getColorClass from "../../utils/getColorClass";
 const Skills = () => {
   const { title, mySkills } = skills;
   return (
@@ -8,10 +9,16 @@ const Skills = () => {
       <h1>{title}</h1>
       <div className="skills-container-grid">
         {mySkills.map((skill, index) => (
-          <div className="skills-card" key={index}>
+          <article className="skills-card" key={index}>
             <h3 className="skills-title">{skill.title}</h3>
-            <p className="skills-stack">{skill.skills}</p>
-          </div>
+            <div className="skills-stack">
+              {skill.skills.map((skill, index) => (
+                <span className={`${getColorClass(skill)}`} key={index}>
+                  {skill}
+                </span>
+              ))}
+            </div>
+          </article>
         ))}
       </div>
     </section>
