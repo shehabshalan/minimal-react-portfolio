a24bb0b45c4cae876dd926998449e0d1541f1be4
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-10 02:40:18 +0200

    contact section completed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { RiSendPlaneFill } from "react-icons/ri";
+import "./Contact.css";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="contact">
+      <div className="contact-me">
+        <h1>
+          <a href="mailto:shehab.shalan@hotmail.com">
+            Contact me
+            <RiSendPlaneFill className="icon" size={30} />
+          </a>
+        </h1>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
