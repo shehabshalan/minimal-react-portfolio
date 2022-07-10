7d09437954ac94918a0aee73e3039a695ecc49ff
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-10 04:14:59 +0200

    skills completed

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import "./Skills.css";
+import { skills } from "../../data/portfolio";
 const Skills = () => {
+  const { title, mySkills } = skills;
   return (
     <section className="skills">
-      <h1>Skills</h1>
+      <h1>{title}</h1>
       <div className="skills-container-grid">
-        <div className="skills-card">
-          <h3 className="skills-title">Languages & Frameworks</h3>
-          <p className="skills-stack">#React #Strapi #Node #Express #MongoDB</p>
-        </div>
-        <div className="skills-card">
-          <h3 className="skills-title">Databases</h3>
-          <p className="skills-stack">#React #Strapi #Node #Express #MongoDB</p>
-        </div>
-        <div className="skills-card">
-          <h3 className="skills-title">Others</h3>
-          <p className="skills-stack">#React #Strapi #Node #Express #MongoDB</p>
-        </div>
+        {mySkills.map((skill, index) => (
+          <div className="skills-card" key={index}>
+            <h3 className="skills-title">{skill.title}</h3>
+            <p className="skills-stack">{skill.skills}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
