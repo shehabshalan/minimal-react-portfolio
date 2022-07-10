7d165ef8c235e190fd038eeb7cb92f8bc4d95cd0
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-10 04:15:06 +0200

    modified data

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,33 +2,41 @@ const about = {
   title: "Hi 👋 , I am Shehab Shalan",
   description:
     "I am a web developer currently pursuing a master's degree. I have worked as a frontend developer for over a year. I have also worked on personal projects involving mainly MERN stack. I have done mostly frontend development, but I am also interested in backend development. I am always open to new challenges.",
+  github: "https://github.com/shehabshalan",
+  linkedin: "https://www.linkedin.com/in/shehabshalan/",
+  cv: "https://firebasestorage.googleapis.com/v0/b/personal-projects-2624c.appspot.com/o/Shehab's%20Resume.pdf?alt=media&token=bc4dcf7f-9417-457c-abdc-f289ca2d0b80",
 };
 
 const projects = {
   title: "Projects",
-  projects: [
+  personalProjects: [
     {
       title: "Online Video Sharing App",
+      short:
+        "Features Firebase authentication, video upload, and video streaming.",
       description:
-        "The app allows users to register/log in, upload a video with a thumbnail and stream it back with the ability to leave comments. Comments can be toggled on and off by the user.",
+        "it is a video-sharing app built using Firebase for authentication, and data persistence. A user can upload a video with a thumbnail and other metadata and stream it back. The app uses other technologies such as MUI for UI. ",
       technologies: "#React.js #MUI #Firebase",
       github: "https://github.com/shehabshalan/online-video-sharing-app",
       demo: "https://videozly.netlify.app/",
     },
     {
       title: "Next.js Blog",
+      short:
+        "Features JWT authentication, headless CMS, and full CRUD operations.",
       description:
-        "it is a Next.js blog site which uses Strapi (a headless CMS) as a backend, and implements concepts such as CSR, SSG and ISR. A user can create, delete, update and read blogs as well as comment and search blogs. The blog site implements full authentication and authorization from Strapi using JWT.",
+        "it is a Next.js blog site built using Strapi (a headless CMS) as a backend and implements concepts such as CSR, SSG, and ISR as well as authentication using JWT. A user can use full CRUD operations including commenting and searching.",
       technologies: "#Next.js #MUI #Strapi #JWT",
       github: "https://github.com/shehabshalan/next-blog",
       demo: "https://next-blog-eta-dun.vercel.app/",
     },
     {
       title: "File Sharing App",
+      short:
+        "Features multi-file upload, sharing links with expiration and document preview.",
       description:
-        "The application is a document library intended to give its users a web based solution to store and share file for a specified duration set by the user.",
-      technologies:
-        "#React.js #Express.js #Node.js #MongoDB #Mongoose #MUI #Cloudinary",
+        "it is a document library (file sharing) built using the MERN stack. Users are able to upload files, keep track of the number of downloads, preview files, and create a sharing link that is set to expire according to the specified time.",
+      technologies: "#React.js #MUI #Express.js #Node.js #MongoDB #Cloudinary",
       github: "https://github.com/shehabshalan/document-library",
       demo: "https://document-library-api.herokuapp.com/",
     },
@@ -40,17 +48,17 @@ const experience = {
   experiences: [
     {
       title: "Frontend Developer",
-      company: "Eta Dun",
+      company: "Trusted KYB Limited",
       duration: "Mar 2021 - Jun 2022",
       description:
-        "I have worked as a frontend developer for Eta Dun, a startup in the field of education. I have worked on the frontend of the website, as well as the backend of the website. I have also worked on the frontend of the mobile app, as well as the backend of the mobile app.",
+        "I worked with **React**, **Material UI** and **TypeScript** on a project based on **atomic design methodology** to deliver new features such as **dashboard** and **risk assessment reports**. I worked with libraries such as **React Query** for API calls, **Formik** and **Yup** for forms and validations. I maintained existing features and and fixed bugs. I implemented **Context API** as well as **custom hooks**. I also worked with a **.NET API** to fetch and manage data on the frontend.",
     },
   ],
 };
 
 const skills = {
   title: "Skills",
-  skills: [
+  mySkills: [
     {
       title: "Languages & Frameworks",
       skills:
